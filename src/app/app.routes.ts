bcbd16ff736818de8515547d041707fdd2ebb228
yonsandovalRM <yonsandoval@gmail.com>
2025-05-29 18:58:12 -0400

    feat: add PricingComponent with initial structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { NotFoundComponent } from './component/not-found/not-found.component';
+import { DashboardComponent } from './component/dashboard/dashboard.component';
+import { PricingComponent } from './component/pricing/pricing.component';
 
 export const routes: Routes = [
   {
@@ -11,6 +13,14 @@ export const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+  },
+  {
+    path: 'pricing',
+    component: PricingComponent,
   },
   {
     path: '**',
---
+++ src/app/component/pricing/pricing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pricing',
+  imports: [],
+  templateUrl: './pricing.component.html',
+  styleUrl: './pricing.component.css'
+})
+export class PricingComponent {
+
+}
